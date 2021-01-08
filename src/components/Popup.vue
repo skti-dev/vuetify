e45e6130280f439d36058c2787925b6d66fb5d99
Augusto Seabra <augusto.seabra00@gmail.com>
2021-01-08 15:46:18 -0300

    adding lesson 25

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,6 +11,14 @@
         <v-form class="px-3">
           <v-text-field prepend-icon="mdi-folder" label="Title" v-model="title"></v-text-field>
           <v-textarea prepend-icon="mdi-pencil" label="Informations about the project" v-model="content"></v-textarea>
+
+          <v-menu>
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field :value="formattedDate" label="Due date" prepend-icon="mdi-calendar" v-bind="attrs" v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+
           <v-btn plain class="success mx-0 mt-3" @click="submit"> Add project </v-btn>
         </v-form>
       </v-card-text>
@@ -19,16 +27,32 @@
 </template>
 
 <script>
+
+import format from "date-fns/format"
+
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      due: null
     }
   },
   methods: {
     submit() {
       console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate() {
+      let aux = ""
+
+      if(this.due){
+        aux = this.due.split("-")
+        aux = new Date(aux[0], (aux[1] - 1), aux[2])
+      }
+
+      return this.due ? format(aux, "Do MMM yyyy") : ""
     }
   }
 }
