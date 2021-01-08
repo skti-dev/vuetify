16205f25c7352e42d8d61de63b88981120e9f269
Augusto Seabra <augusto.seabra00@gmail.com>
2021-01-08 17:10:04 -0300

    adicionando aula 31

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -16,17 +16,13 @@
 </template>
 
 <script>
+
+import db from "@/fb"
+
 export default {
   data() {
     return {
-      projects: [
-        { title: "Design a new website", person: "The Net Ninja",  due: "1st Jan 2019", status: "ongoing", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-        { title: "Design a new website 2", person: "The Net Ninja",  due: "1st Jan 2019", status: "ongoing", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-        { title: "Design a new website 3", person: "The Net Ninja",  due: "1st Jan 2019", status: "ongoing", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-        { title: "Code up the homepage", person: "Chun Li",  due: "10th Jan 2019", status: "complete", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-        { title: "Design video thumbnails", person: "Ryu",  due: "20th Dec 2018", status: "complete", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-        { title: "Create a community forum", person: "Gouken",  due: "20th Oct 2018", status: "overdue", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores natus, corrupti delectus dolorum quibusdam ipsum voluptas pariatur tempore quaerat, sit earum consectetur eligendi necessitatibus voluptate praesentium quod! Qui, nihil ratione." },
-      ]
+      projects: []
     }
   },
   computed: {
@@ -35,6 +31,20 @@ export default {
         return project.person == "The Net Ninja"
       })
     }
+  },
+  created() {
+    db.collection("projects").onSnapshot(res => {
+      const changes = res.docChanges()
+
+      changes.forEach(change => {
+        if(change.type === "added"){
+          this.projects.push({
+            ...change.doc.data(),
+            id: change.doc.id
+          })
+        }
+      })
+    })
   }
 }
 </script>
